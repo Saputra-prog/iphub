115eab750a9614970ca968fce0a0924d51432368
Saputra-prog <m4t11928@gmail.com>
2026-08-11 21:30:39 +0700

    Final iphub

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Navbar from './components/navbar'
 import Desk from './Home/desk/page'
 import Lokasi from './Home/lokasi/page'
 import Berita from './Home/berita/page'
+import Komentar from './Home/komentar/page'
+import Bisnis from './Home/bisnis/page'
 import Footer from './components/footer'
 
 
@@ -16,8 +18,14 @@ function page() {
       <div id="desk">
       <Desk />
       </div>
+      <div id="bisnis">
+      <Bisnis />
+      </div>
       <div id="lokasi">
       <Lokasi />
+      </div>
+      <div id="komentar">
+      <Komentar />
       </div>
       <div id="berita">
       <Berita />
